bf811324f9ec1d0f11ba72e263c70ff01e3cbc49
Georgina <georginacarvajal@colegioguanenta.edu.co>
2023-03-23 15:11:52 -0500

    Se creo el primer ejemplo

---
+++ CursoPython.py
@@ -0,0 +1,12 @@
+# While
+
+#x = 0
+#while x < 10:
+#   print ("El valor de x es :",x)
+#   x += 1
+
+num = int(input("Escribe un numero positivo"))
+while num < 0:
+    print("Este es numero negativo, prueba de nuevo")
+    num = int(input("Escribe un numero positivo"))
+print ("El numero es: ",num)
